58f04b49f18f86c71b48cf38f2cd1317aa82d9ec
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2021-02-16 23:16:03 +0000

    feat: first working version that added data to a goal

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const guid = require('./guid.js');
+var beeminder = require('beeminder');
+var auth_token = '';
+var bm = beeminder(auth_token);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+	
+	// Output timestamp
+	const time = (new Date()).toTimeString();
+	core.setOutput("time", time);
+	
+	// Get comment input from user
+	const comment = core.getInput('comment');
+
+	bm.createDatapoint('multigitminder', {	
+	  value: 1, // {type: Number, required: true},
+	  timestamp: new Date().valueOf() / 1000, // {type: Number, default: now},
+	  comment: '${comment}', // `comment` input defined in action metadata file
+	  sendmail: false, // if you want the user to be emailed
+	  // requestid allows you to run command again without creating duplicate datapoints
+	  requestid: guid.createGuid(),
+	}, function (err, result) {
+		
+		// Output error or result from API
+		console.log(err || result); 
+	});
+
 } catch (error) {
-  core.setFailed(error.message);
+	core.setFailed(error.message);
 }
+
+// try {
+  // // `who-to-greet` input defined in action metadata file
+  // const nameToGreet = core.getInput('who-to-greet');
+  // console.log(`Hello ${nameToGreet}!`);
+  // const time = (new Date()).toTimeString();
+  // core.setOutput("time", time);
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+// } catch (error) {
+  // core.setFailed(error.message);
+// }
