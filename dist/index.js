80f268c220d485f89e93eae728bc2296d09ab486
Hayden MacDonald <hayden.macdonald.8778@gmail.com>
2021-02-27 18:14:20 +0000

    fix: changes to ensure proper handling of user inputs, moved Output Time into callback

--- dist/index.js
+++ dist/index.js
@@ -6638,51 +6638,44 @@ function wrappy (fn, cb) {
 /***/ 3348:
 /***/ ((__unused_webpack_module, __unused_webpack_exports, __nccwpck_require__) => {
 
+// Dependencies
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 const guid = __nccwpck_require__(7900);
 var beeminder = __nccwpck_require__(7253);
-var auth_token = '';
-var bm = beeminder(auth_token);
 
 try {
 	
-	// Output timestamp
-	const time = (new Date()).toTimeString();
-	core.setOutput("time", time);
-	
-	// Get comment input from user
+	// Get auth_token, goal, and comment input from user
+	const auth_token = core.getInput('auth_token');
+	const goal = core.getInput('goal');
 	const comment = core.getInput('comment');
 
-	bm.createDatapoint('multigitminder', {	
+	// Access API with token
+	var bm = beeminder(auth_token);
+
+	// Create a data point for the goal
+	bm.createDatapoint(`${goal}`, {	
 	  value: 1, // {type: Number, required: true},
 	  timestamp: new Date().valueOf() / 1000, // {type: Number, default: now},
-	  comment: '${comment}', // `comment` input defined in action metadata file
+	  comment: `${comment}`, // `comment` input defined in action metadata file
 	  sendmail: false, // if you want the user to be emailed
 	  // requestid allows you to run command again without creating duplicate datapoints
 	  requestid: guid.createGuid(),
 	}, function (err, result) {
 		
 		// Output error or result from API
-		console.log(err || result); 
+		console.log(err || result);
+
+		// Output timestamp
+		const time = (new Date()).toTimeString();
+		core.setOutput("time", time);
+
 	});
 
 } catch (error) {
 	core.setFailed(error.message);
 }
-
-// try {
-  // // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-// } catch (error) {
-  // core.setFailed(error.message);
-// }
 
 /***/ }),
 
--- src/action.js
+++ src/action.js
@@ -1,47 +1,38 @@
+// Dependencies
 const core = require('@actions/core');
 const github = require('@actions/github');
 const guid = require('./guid.js');
 var beeminder = require('beeminder');
-// var auth_token = '';
-var bm = beeminder(auth_token);
 
 try {
 	
-	// Output timestamp
-	const time = (new Date()).toTimeString();
-	core.setOutput("time", time);
-	
-	// Get comment input from user
+	// Get auth_token, goal, and comment input from user
 	const auth_token = core.getInput('auth_token');
 	const goal = core.getInput('goal');
 	const comment = core.getInput('comment');
 
-	bm.createDatapoint('multigitminder', {	
+	// Access API with token
+	var bm = beeminder(auth_token);
+
+	// Create a data point for the goal
+	bm.createDatapoint(`${goal}`, {	
 	  value: 1, // {type: Number, required: true},
 	  timestamp: new Date().valueOf() / 1000, // {type: Number, default: now},
-	  comment: '${comment}', // `comment` input defined in action metadata file
+	  comment: `${comment}`, // `comment` input defined in action metadata file
 	  sendmail: false, // if you want the user to be emailed
 	  // requestid allows you to run command again without creating duplicate datapoints
 	  requestid: guid.createGuid(),
 	}, function (err, result) {
 		
 		// Output error or result from API
-		console.log(err || result); 
+		console.log(err || result);
+
+		// Output timestamp
+		const time = (new Date()).toTimeString();
+		core.setOutput("time", time);
+
 	});
 
 } catch (error) {
 	core.setFailed(error.message);
 }
-
-// try {
-  // // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet');
-  // console.log(`Hello ${nameToGreet}!`);
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`);
-// } catch (error) {
-  // core.setFailed(error.message);
-// }
